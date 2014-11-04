9fa97ca9bf9145410408ab06e7666459fe86b999
Mart Lubbers <mart@martlubbers.net>
2014-11-04 12:49:00 +0100

    updated readme and instructions

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ PREFIX="/usr/local"
 MANPREFIX="$PREFIX/share/man"
 BINDIR="$PREFIX/bin"
 
-install -C -m 644 -o root -g root ./vocatrainer "$BINDIR/"
+install -C -m 755 ./vocatrainer "$BINDIR/"
 [ ! -f "./vocatrainer.1.gz" ] && gzip -9qc ./vocatrainer.1 > ./vocatrainer.1.gz
-install -C -m 644 -o root -g root ./vocatrainer.1.gz "$MANPREFIX/man1/"
+install -C -m 755 ./vocatrainer.1.gz "$MANPREFIX/man1/"
 mandb -q
