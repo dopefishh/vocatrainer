0eded786d57a4b2b6d5f2ea98bbf533da4803292
Mart Lubbers <mart@martlubbers.net>
2014-11-11 07:56:49 +0100

    Fixed some things, namely errors writing

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ BINDIR="$PREFIX/bin"
 
 install -C -m 755 ./vocatrainer "$BINDIR/"
 [ ! -f "./vocatrainer.1.gz" ] && gzip -9qc ./vocatrainer.1 > ./vocatrainer.1.gz
+mkdir -vp "$MANPREFIX/man1"
 install -C -m 755 ./vocatrainer.1.gz "$MANPREFIX/man1/"
 mandb -q
